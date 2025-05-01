438011bbccc756288cca6a10f5be03ef92a47acd
bojanstrkovski-21 <bojanstrkovski.21@gmail.com>
2025-05-01 13:50:14 +0200

    initial commit nlogout

---
+++ build-for-repo.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Define color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+# Function to print colored output
+print_color() {
+    printf "${!1}%s${NC}\n" "$2"
+}
+
+# ## Install nim language
+# print_color "YELLOW" "Installing nim..."
+# if sudo pacman -S nim --noconfirm --needed; then
+#     print_color "GREEN" "nim installed successfully."
+# else
+#     print_color "RED" "Failed to install nim. Please install it manually and rerun this script."
+#     exit 1
+# fi
+
+# ## Install required Nim modules
+# print_color "YELLOW" "Installing required Nim modules..."
+# if yes | nimble install parsetoml && yes | nimble install nigui; then
+#     print_color "GREEN" "Modules installed successfully."
+# else
+#     print_color "RED" "Failed to install required modules. Please check your internet connection and try again."
+#     exit 1
+# fi
+
+# Compile nlogout
+print_color "YELLOW" "Compiling nlogout..."
+if nim compile --define:release --opt:size --app:gui --outdir=/home/bojanstrko/DATA/pkgs/archlinux-nlogout/etc/skel/.config/nlogout/bin/ /home/bojanstrko/DATA/pkgs/archlinux-nlogout/etc/skel/.config/nlogout/src/nlogout.nim; then
+    print_color "GREEN" "nlogout compiled successfully."
+else
+    print_color "RED" "Failed to compile nlogout. Please check the error messages above."
+    exit 1
+fi
